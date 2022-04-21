e0c62235ea8f51dd4b023b2ecb28a3125209692d
bsenyayla <56520710+bsenyayla@users.noreply.github.com>
2022-04-21 10:49:41 -0500

    upd
    upd

--- src/main/java/com/eregistrar/miueregistrar/exceptions/CourseRegisterException.java
+++
@@ -1,7 +0,0 @@
-package com.eregistrar.miueregistrar.exceptions;
-
-public class CourseRegisterException extends Exception {
-    public CourseRegisterException(String errorMessage) {
-        super(errorMessage);
-    }
-}
